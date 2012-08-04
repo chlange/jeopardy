b8f6bc7a7e2d63aa442c6f278210231005afb8c6
Christian Lange <chlange@htwg-konstanz.de>
2012-08-04 17:30:48 +0200

    Revert "Removed removeMocErrors.sh since its not needed anymore"
    This reverts commit c8d7bab3680dccbde73c1eb3d1f28cc6618c2638.

---
+++ removeMocErrors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Not needed anymore!
+
+# Remove error defines in moc_* files
+# Qt version 4.8 produces errors which can be fixed with this workaround
+# Not the best solution but it works for version 4.8
+
+# sed -i '13,16 s/^/\/\//' moc*.cpp
